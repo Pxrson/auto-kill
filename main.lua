691bec0464416cd8a571574cea60b42dc29868d8
pxrson <159809309+Pxrson@users.noreply.github.com>
2025-08-06 16:41:05 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,1 @@
-local plrs = game:GetService("Players")
-local ts = game:GetService("TeleportService")
-
-queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Pxrson/auto-kill/refs/heads/main/auto%20kill.lua'))()")
-
-if #plrs:GetPlayers() <= 5 then
-    ts:Teleport(game.PlaceId)
-    return
-end
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Pxrson/auto-kill/refs/heads/main/auto%20kill.lua'))()
+-- fixing
