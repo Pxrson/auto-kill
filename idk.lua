dbc013a11b466d75cd532dd5faa184b85dc7ee80
pxrson <159809309+Pxrson@users.noreply.github.com>
2025-08-07 02:18:58 -0500

    Update idk.lua

--- idk.lua
+++ idk.lua
@@ -1,27 +1,13 @@
--- fixing
-local ts = game:GetService("TeleportService")
-local plrs = game:GetService("Players")
-local id = game.PlaceId
-
-local function loadAutoKill()
-    local scriptCode = game:HttpGet("https://raw.githubusercontent.com/Pxrson/auto-kill/main/auto%20kill.lua")
-    loadstring(scriptCode)()
-end
-
-local function hop()
-    if #plrs:GetPlayers() < 5 then
+while true do
+    if #game:GetService("Players"):GetPlayers() < 5 then
         for _, obj in ipairs(game:GetService("ReplicatedFirst"):GetChildren()) do
             if obj:IsA("Script") or obj:IsA("LocalScript") then
                 obj:Destroy()
             end
         end
-        ts:Teleport(id, plrs.LocalPlayer)
+        game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
     else
-        loadAutoKill()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxrson/auto-kill/main/auto%20kill.lua"))()
     end
-end
-
-while true do
-    hop()
     task.wait(10)
 end
