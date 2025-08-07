7e05cae6a40e377f99079e12c975c5fa9fbe6de1
pxrson <159809309+Pxrson@users.noreply.github.com>
2025-08-06 21:47:08 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,27 @@
 -- fixing
+local ts = game:GetService("TeleportService")
+local plrs = game:GetService("Players")
+
+local id = game.PlaceId
+
+local function loadAutoKill()
+    game:GetObjects("https://raw.githubusercontent.com/Pxrson/auto-kill/refs/heads/main/auto%20kill.lua")[1].Parent = game:GetService("ReplicatedFirst")
+end
+
+local function hop()
+    if #plrs:GetPlayers() < 5 then
+        for _, obj in ipairs(game:GetService("ReplicatedFirst"):GetChildren()) do
+            if obj:IsA("Script") or obj:IsA("LocalScript") then
+                obj:Destroy()
+            end
+        end
+        ts:Teleport(id, plrs.LocalPlayer)
+    else
+       loadAutoKill()
+    end
+end
+
+while true do
+    hop()
+    task.wait(10)
+end
