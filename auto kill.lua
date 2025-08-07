b5b1760eb1bced5000690aace32359ef96ac6a38
pxrson <159809309+Pxrson@users.noreply.github.com>
2025-08-07 18:37:01 -0500

    Update auto kill.lua

--- auto kill.lua
+++ auto kill.lua
@@ -2,77 +2,33 @@
 local plrs = game:GetService("Players")
 local rs = game:GetService("RunService")
 local lp = plrs.LocalPlayer
-local animIds = {"rbxassetid://3638729053","rbxassetid://3638749874","rbxassetid://3638767427","rbxassetid://102357151005774"}
 
-local char, hum, hand, punch, anim
-local targets = {}
-local lastUpdate = 0
-
-local function updateCache()
-    char = lp.Character
-    if char then
-        hum = char:FindFirstChild("Humanoid")
-        hand = char:FindFirstChild("LeftHand") or char:FindFirstChild("Left Arm")
-        punch = char:FindFirstChild("Punch")
-        anim = hum and hum:FindFirstChild("Animator")
-    end
-end
-
-local function updateTargets()
-    targets = {}
-    for _, plr in pairs(plrs:GetPlayers()) do
-        if plr ~= lp and plr.Character then
-            local tgtHum = plr.Character:FindFirstChild("Humanoid")
-            local tgtRp = plr.Character:FindFirstChild("HumanoidRootPart")
-            if tgtHum and tgtHum.Health > 0 and tgtRp then
-                targets[#targets + 1] = {plr = plr, rp = tgtRp}
-            end
-        end
-    end
-end
-
-local function autokill()
-    local tick = os.clock()
-    
-    if tick - lastUpdate > 0.1 then
-        updateCache()
-        updateTargets()
-        lastUpdate = tick
-    end
-    
-    if not punch then
-        local tool = lp.Backpack:FindFirstChild("Punch")
-        if tool and hum then
-            hum:EquipTool(tool)
-        end
-        return
-    end
-    
-    punch.attackTime.Value = 0
-    
-    for i = 1, #targets do
-        local target = targets[i]
-        punch:Activate()
-        firetouchinterest(target.rp, hand, 0)
-        firetouchinterest(target.rp, hand, 1)
-    end
-    
-    if anim then
-        for _, trk in pairs(anim:GetPlayingAnimationTracks()) do
-            for _, id in pairs(animIds) do
-                if trk.Animation and trk.Animation.AnimationId == id then
-                    trk:Stop()
-                    break
+spawn(function()
+    while true do
+        if lp.Character and lp.Character:FindFirstChild("Punch") then
+            local punch = lp.Character.Punch
+            local hand = lp.Character:FindFirstChild("LeftHand") or lp.Character:FindFirstChild("Left Arm")
+            
+            punch.attackTime.Value = 0
+            
+            for _, plr in pairs(plrs:GetPlayers()) do
+                if plr ~= lp and plr.Character and plr.Character.Humanoid.Health > 0 then
+                    punch:Activate()
+                    firetouchinterest(plr.Character.HumanoidRootPart, hand, 0)
+                    firetouchinterest(plr.Character.HumanoidRootPart, hand, 1)
                 end
             end
+            
+            for _, trk in pairs(lp.Character.Humanoid.Animator:GetPlayingAnimationTracks()) do
+                local id = trk.Animation and trk.Animation.AnimationId
+                if id and (id:find("3638729053") or id:find("3638749874") or id:find("3638767427") or id:find("102357151005774")) then
+                    trk:Stop()
+                end
+            end
+        elseif lp.Character and lp.Backpack:FindFirstChild("Punch") then
+            lp.Character.Humanoid:EquipTool(lp.Backpack.Punch)
         end
+        
+        game:GetService("TweenService"):Create(game.Workspace, TweenInfo.new(0), {}):Play()
     end
-end
-
-updateCache()
-rs.Heartbeat:Connect(autokill)
-
-lp.CharacterAdded:Connect(function()
-    wait(0.1)
-    updateCache()
 end)
