18bb50f7732d4ec0dc1873f0df2de00a496d6094
pxrson <159809309+Pxrson@users.noreply.github.com>
2025-08-07 18:08:23 -0500

    Update auto kill.lua

--- auto kill.lua
+++ auto kill.lua
@@ -3,8 +3,8 @@ local plrs = game:GetService("Players")
 local rs = game:GetService("RunService")
 local lp = plrs.LocalPlayer
 
-rs.Heartbeat:Connect(function()
-    pcall(function()
+spawn(function()
+    while true do
         if lp.Character and lp.Character:FindFirstChild("Punch") then
             local punch = lp.Character.Punch
             local hand = lp.Character:FindFirstChild("LeftHand") or lp.Character["Left Arm"]
@@ -19,25 +19,19 @@ rs.Heartbeat:Connect(function()
             end
             
             for _, trk in pairs(lp.Character.Humanoid.Animator:GetPlayingAnimationTracks()) do
-                if trk.Animation then
-                    local id = trk.Animation.AnimationId
-                    if id:find("3638729053") or id:find("3638749874") or id:find("3638767427") or id:find("102357151005774") then
-                        trk:Stop()
-                    end
-                end
+                trk:Stop()
             end
         elseif lp.Character and lp.Backpack:FindFirstChild("Punch") then
             lp.Character.Humanoid:EquipTool(lp.Backpack.Punch)
         end
-    end)
+    end
 end)
 
-rs.Stepped:Connect(function() -- second one so this bitch doesnt stop
-    pcall(function()
+spawn(function() -- backup so ts bih dont stop
+    while true do
         if lp.Character and lp.Character:FindFirstChild("Punch") then
             local punch = lp.Character.Punch
             local hand = lp.Character:FindFirstChild("LeftHand") or lp.Character["Left Arm"]
-            punch.attackTime.Value = 0
             
             for _, plr in pairs(plrs:GetPlayers()) do
                 if plr ~= lp and plr.Character and plr.Character.Humanoid.Health > 0 then
@@ -47,5 +41,5 @@ rs.Stepped:Connect(function() -- second one so this bitch doesnt stop
                 end
             end
         end
-    end)
+    end
 end)
